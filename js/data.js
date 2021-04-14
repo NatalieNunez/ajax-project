31c528bf5a6a175d024011a8ffc0ead035ba73a3
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-04-14 18:56:37 +0000

    listen for click events on buttons, ensured that clicking on a button shows the correct view

--- js/data.js
+++ js/data.js
@@ -38,3 +38,51 @@ var data = {
 };
 
 // console.log(data);
+
+var $homePage = document.querySelector('.home-page');
+var $generalSetup = document.querySelector('.general-jokes-setup');
+var $generalPunchline = document.querySelector('.general-jokes-punchline');
+var $programmingSetup = document.querySelector('.programming-jokes-setup');
+var $programmingPunchline = document.querySelector('.programming-jokes-punchline');
+
+var $chooseGeneral = document.querySelector('.general-jokes-button');
+var $chooseProgramming = document.querySelector('.programming-jokes-button');
+var $tellMeGeneral = document.querySelector('.tell-me-general');
+var $tellMeProgramming = document.querySelector('.tell-me-programming');
+var $anotherGeneral = document.querySelector('.another-general');
+var $anotherProgramming = document.querySelector('.another-programming');
+
+function handleClick(event) {
+  // console.log(event.target);
+  if (event.target === $chooseGeneral) {
+    $homePage.classList.add('hidden');
+    $generalSetup.classList.remove('hidden');
+  }
+  if (event.target === $chooseProgramming) {
+    $homePage.classList.add('hidden');
+    $programmingSetup.classList.remove('hidden');
+  }
+  if (event.target === $tellMeGeneral) {
+    $generalSetup.classList.add('hidden');
+    $generalPunchline.classList.remove('hidden');
+  }
+  if (event.target === $tellMeProgramming) {
+    $programmingSetup.classList.add('hidden');
+    $programmingPunchline.classList.remove('hidden');
+  }
+  if (event.target === $anotherGeneral) {
+    $generalPunchline.classList.add('hidden');
+    $generalSetup.classList.remove('hidden');
+  }
+  if (event.target === $anotherProgramming) {
+    $programmingPunchline.classList.add('hidden');
+    $programmingSetup.classList.remove('hidden');
+  }
+}
+
+$chooseGeneral.addEventListener('click', handleClick);
+$chooseProgramming.addEventListener('click', handleClick);
+$tellMeGeneral.addEventListener('click', handleClick);
+$tellMeProgramming.addEventListener('click', handleClick);
+$anotherGeneral.addEventListener('click', handleClick);
+$anotherProgramming.addEventListener('click', handleClick);
